0777afd40e6fb9a82e7e1edf1c9689da6b57a17c
Manuel Wittmann <manuel.wittmann@posteo.de>
2024-10-01 18:01:53 +0200

    chore: Update npm dependencies and remove unused SCSS files

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
-<script setup>
-import Header2 from "~/components/Header2.vue";
-</script>
+<script setup></script>
 
-<template>
-  <div class="flex flex-col min-h-screen">
-    <Header class="designer-portfolio-header">
-      <template v-slot:logo>
-        <img src="/images/logo/logo-light.webp" alt="Creative Agency Logo" />
-      </template>
-    </Header>
-    <Header2 />
-    <main class="flex-grow container mx-auto p-4">
-      <slot />
-    </main>
-    <footer class="bg-gray-800 text-white p-4 mt-auto">
-      <div class="container mx-auto text-center">
-        &copy; 2024 Manuel Wittmann
-      </div>
-    </footer>
-  </div>
-</template>
+<template></template>
 
 <style scoped>
 /* Add any additional styles if needed */
