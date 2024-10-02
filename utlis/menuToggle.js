378cfb8895dc268e54d3da5c254264727a2d474e
Manuel Wittmann <manuel.wittmann@posteo.de>
2024-10-02 17:03:01 +0200

    chore: Add smooth scrolling functionality and scrollspy feature

---
+++ utlis/menuToggle.js
@@ -0,0 +1,34 @@
+export function menuOpen(e) {
+  e.preventDefault();
+  document.querySelector(".popup-mobile-menu")?.classList.add("menu-open");
+  document.documentElement.style.overflow = "hidden";
+}
+
+export function closeMobileMenu(e) {
+  e.preventDefault();
+  document.querySelector(".popup-mobile-menu")?.classList.remove("menu-open");
+
+  document.documentElement.style.overflow = "";
+}
+export function mobileMenuOpen(e) {
+  if (e.target === this) {
+    document.querySelector(".popup-mobile-menu")?.classList.remove("menu-open");
+    document.documentElement.style.overflow = "";
+  }
+}
+// document
+//   .querySelectorAll(".popup-mobile-menu .has-droupdown > a")
+//   ?.forEach(function (item) {
+//     item?.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       var submenu = this.nextElementSibling;
+//       submenu?.classList.toggle("active");
+//       if (submenu?.classList.contains("active")) {
+//         submenu.style.display = "block";
+//       } else {
+//         submenu.style.display = "none";
+//       }
+//       this?.classList.toggle("open");
+//       document.documentElement.style.overflow = "";
+//     });
+//   });
