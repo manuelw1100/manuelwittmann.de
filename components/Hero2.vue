e24747982fdafc2e26c32e658bbe670649d1cf0f
Manuel Wittmann <manuel.wittmann@posteo.de>
2024-10-09 12:13:23 +0200

    feat: Update Hero2 component subtitle and description

--- components/Hero2.vue
+++ components/Hero2.vue
@@ -3,22 +3,20 @@
     <div class="slide slider-style-1">
       <div class="container">
         <div class="row row--30 align-items-center">
-          <div class="order-1 order-lg-1 col-lg-7 mt_md--50 mt_sm--50 mt_lg--30">
+          <div class="order-1 order-lg-1 col-lg-8 mt_md--50 mt_sm--50 mt_lg--30">
             <div class="content">
               <div class="inner">
-                <span class="subtitle">Frontend-Lösungen | Web-Apps | Beratung & Entwicklung</span>
+                <span class="subtitle">Web-Lösungen | Beratung & Entwicklung</span>
                 <h1 class="title">
-                  Frontend-Entwicklung mit <span>Vue.js</span><br />
+                  <span>Erfolgreich</span> mit einer modernen, performanten Web-Anwendung<br />
                 </h1>
 
                 <div>
                   <p class="description">
-                    Moderne Frontend- und Web-Lösungen mit Vue.js & Co.
-                    Profitiere von 6 Jahren Vue-Spezialisierung
-                    und
-                    über 11 Jahren Frontend-Erfahrung.
+                    Ich entwickle Deine Weblösung – flexibel, skalierbar und barrierefrei, damit sie für alle nutzbar
+                    ist. Perfekt für Dein Wachstum. Profitiere von mehr als 11 Jahren Erfahrung.
                   </p>
-                  <a href="#" class="rn-btn btn-theme">
+                  <a href="#" class="rn-btn btn-theme mt-5">
                     <span>Kostenloses Erstgespräch vereinbaren</span>
                     <i data-feather="arrow-right"></i>
                   </a>
@@ -57,5 +55,9 @@ onUnmounted(() => {
 <style lang="scss" scoped>
 .slide {
   padding-bottom: 0;
+}
+
+.slide .content .inner {
+  margin-bottom: 100px;
 }
 </style>
