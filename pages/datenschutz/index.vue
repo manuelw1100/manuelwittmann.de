ddb5040490e3d7caa9ba9c7a153ae771ea9d5701
Manuel Wittmann <manuel.wittmann@posteo.de>
2024-11-06 20:56:39 +0100

    feat: Add DataSplitter, Datenschutz, and Impressum components

---
+++ pages/datenschutz/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="white-version">
+    <Header :navItems="navItemsClean" />
+    <div class="main-page-wrapper">
+      <div id="about" class="rn-about-area rn-section-gap">
+        <div class="container">
+          <div class="row">
+            <div class="col-10 mx-auto">
+              <div class="contant">
+                <div class="section-title text-left mb-4">
+                  <h1 class="title">Datenschutz</h1>
+                </div>
+
+                <Datenschutz />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <Footer2 />
+  </div>
+</template>
+<script setup lang="ts">
+useHead({
+  meta: [{ hid: "robots", name: "robots", content: "noindex" }],
+});
+import { navItemsClean } from "~/data/menu";
+import Header from "~/components/header/Header.vue";
+import Datenschutz from "~/components/Datenschutz.vue";
+</script>
+<style scoped>
+h1,
+h2 {
+
+  color: var(--color-body-white);
+
+}
+</style>
